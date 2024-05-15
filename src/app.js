0463dda45d90701eaf995a6ba5c367a4ec8950a6
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-05-15 18:06:21 +0000

    Recargar pag autom

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function generador() {
   //write your code here
+  let excusaFinal = "";
+
+  let parrafo = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[Math.floor(Math.random() * 4)];
+  let accion = action[Math.floor(Math.random() * 4)];
+  let que = what[Math.floor(Math.random() * 3)];
+  let cuando = when[Math.floor(Math.random() * 5)];
+
+  excusaFinal += quien + " " + accion + " " + que + " " + cuando;
+  console.log(excusaFinal);
+
+  parrafo.innerHTML = excusaFinal;
+
   console.log("Hello Rigo from the console!");
-};
+}
+// Permite que la página se actualice automáticamente cada 7 segundos
+setInterval(generador, 7000);
